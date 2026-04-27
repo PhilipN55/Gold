33f0bf8c827736743fb5286b6ca942ed0b0e1d26
PhilipN55 <228013432+PhilipN55@users.noreply.github.com>
2026-04-27 17:13:35 +0200

    0.41

--- Gold Gold Gold.py
+++ Gold Gold Gold.py
@@ -26,13 +26,23 @@ RarityChans = {
     "Covert": 2,
     "Gold": 0.25,
 }
+RarityValue = {
+    "Consumer": 10,
+    "Industrial": 25,
+    "Mil-Spec": 100,
+    "Restricted": 300,
+    "Classified": 800,
+    "Covert": 2000,
+    "Gold": 10000
+}
 rarity_rank = {
     "Consumer": 1,
     "Industrial": 2,
     "Mil-Spec": 3,
     "Restricted": 4,
     "Classified": 5,
-    "Covert": 6
+    "Covert": 6,
+    "Gold": 7
 }
 
 weapons = [
@@ -50,26 +60,34 @@ weapons = [
 
 drops = []
 sorted_drops = []
-
+scroll_y = 0
 top10 = []
 weapon = ""
 rarity = ""
+worth = ""
+inventory = False
+loot = []
 floatV = 0
 text_color = (255, 255, 255)
+#inventory
+#Rect = pygame.Rect(50, 100, 140, 100)
+RectColor = (25, 25, 25)
 
+
+#------------------------------#
 #"r" = read
 with open("top10.json", "r") as f:
     top10 = json.load(f)
-with open("top10.json", "r") as f:
-    drops = json.load(f)
 
 print(sorted_drops)
+text1 = font.render("inventory = I", True,(255, 255, 255))
+
 while running:
     screen.fill((0, 0, 0))
+    screen.blit(text1, (620, 40))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 floatV = round(random.uniform(0.00, 1.00), 3)
@@ -81,43 +99,69 @@ while running:
                 #choices() kan man sätta vikter/chanser på inte choice()
                 #[0] konverterar ["rarity"] till "rarity"
                 rarity = random.choices(rarities, weights=chans, k=1)[0]
+                #värde kalkylator, int = avrunda
+                value = int(RarityValue[rarity] * (1 + (1 - floatV) * 2))
                 text_color = Rarity[rarity]
                 drops.append({
                     "name": weapon,
                     "rarity": rarity,
-                    "float": floatV
+                    "float": floatV,
+                    "worth": value
+                })
+                loot.append({
+                    "name": weapon,
+                    "rarity": rarity,
+                    "float": floatV,
+                    "worth": value
                 })
                 #key är hur det sorteras
                 #lambda x är anonym funktion utan namn
                 #-rarity_rank gör att det blir rätt ordning högst-lägst
                 #tuple är en lista som inte kan ändras
                 #om rarity e samma sortera då på float
-                sorted_drops = sorted(drops,key=lambda x: (-rarity_rank[x["rarity"]], x["float"]))
+                drops_to_sort = (drops+top10)
+                sorted_drops = sorted(drops_to_sort,key=lambda x: (-rarity_rank[x["rarity"]], x["float"]))
                 top10 = sorted_drops[:10]
+                drops = []
                 #with ser till att filen stängs korrekt
                 #"w" = write (skrivläge)
                 #indent = mer luftigt i json filen
                 #.json.dump konverterar kod till dict i json och sparar det
                 with open("top10.json", "w") as f: json.dump(top10, f, indent=2)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_i:
+                if inventory == False:
+                    inventory = True
+                else:
+                    inventory = False
 
-    text = font.render(f"{weapon}: float {floatV}",True,text_color)
-    text_rect = text.get_rect(center=(400, 120))
-    screen.blit(text, text_rect)
+    if inventory == False:
+        text = font.render(f"{weapon}: float {floatV}",True,text_color)
+        text_rect = text.get_rect(center=(400, 120))
+        screen.blit(text, text_rect)
 
+        y  = 200
+        #enumerate = (i, item)
+        for i, item in enumerate(top10):
+            x = 150
+            leaderboard_name = font.render(f"{1+i}.{item["name"]}",True,Rarity[item["rarity"]])
+            leaderboard_float = font.render(f"value {item["worth"]}kr", True,(255, 255, 255))
 
-    y  = 200
-    for i, item in enumerate(top10):
-        x = 150
-        leaderboard_name = font.render(f"{1+i}.{item["name"]}",True,(255,255,255))
-        leaderboard_rarity = font.render(f"{item["rarity"]}", True,Rarity[item["rarity"]])
-        leaderboard_float = font.render(f"{item["float"]}", True,(255, 255, 255))
+            screen.blit(leaderboard_name,(150,y))
+            screen.blit(leaderboard_float,(600,y))
+            y += 30
+    # inventory
+    if inventory == True:
+        for i, item in enumerate(loot):
+            if i < 20:
+                rutor = 5 # antal rutor
+                kolumn = i % rutor # % räkna om inom 0-4
+                rad = i // rutor # // heltalsdivision
 
-        screen.blit(leaderboard_name,(150,y))
-        x += leaderboard_name.get_width()
-        screen.blit(leaderboard_rarity,(x+10,y))
-
-        screen.blit(leaderboard_float,(600,y))
-        y += 30
+                x = 50 + kolumn * 160
+                y = 100 + rad * 120
+                pygame.draw.rect(screen, RectColor, pygame.Rect(x-40, y, 140, 100), border_radius=15)
+    # ---------------------------------------------------------------------------------------------------#
 
 
     pygame.display.update()
